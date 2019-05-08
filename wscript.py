0e4603cf2b113cd95f594aa3a2242b7cc61a4ffd
Bobby de Vos <bobby_devos@sil.org>
2019-05-08 10:39:16 -0600

    Cleanup from changing Latin glyph import process

--- wscript.py
+++ wscript.py
@@ -1,5 +1,2 @@
 faces = ('Vayalar', 'Malabar') # Kalyani
-facesLegacy = ('vaya', 'mala') # kaly
-styles = ('-R', '-B', '-I', '-BI')
 stylesName = ('Regular', 'Bold', 'Italic', 'Bold Italic')
-stylesLegacy = ('', 'bd', 'i', 'bi')
