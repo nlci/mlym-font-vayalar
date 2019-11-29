f7752e721ff62805ba019b427d0c174bed75250b
Bobby de Vos <bobby_devos@sil.org>
2019-11-29 15:44:20 -0700

    Ensure basic characters (Latin and common Indic) are present

--- tools/encoding.py
+++ tools/encoding.py
@@ -8,10 +8,8 @@ ufo = sys.argv[1]
 font = OpenFont(ufo)
 
 # Modify UFO
-lettera = font['u0D05']
-lettera.name = 'avowel'
 
 # Save UFO
-font.changed()
-font.save()
-font.close()
+# font.changed()
+# font.save()
+# font.close()
--- tools/import.bash
+++ tools/import.bash
@@ -5,7 +5,7 @@ style="$2"
 ufo="$3"
 
 devaf="Maurya"
-# tamlf="Vaigai"
+tamlf="Vaigai"
 
 psfcopyglyphs -f --rename rename --unicode usv -i ../cs/panini/main4mlym.csv -s "${deva}/${devaf}-${style}.ufo" ${ufo}
-# psfcopyglyphs -f --rename rename --unicode usv -i ../cs/thiruvalluvar/main.csv -s "${taml}/${tamlf}-${style}.ufo" ${ufo}
+psfcopyglyphs -f --rename rename --unicode usv -i ../cs/thiruvalluvar/main4mlym.csv -s "${taml}/${tamlf}-${style}.ufo" ${ufo}
